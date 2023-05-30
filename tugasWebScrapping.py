6b2b86a0573891ddde85f87f64b333c9318c8e7c
laainra <lailaar77@gmail.com>
2023-05-30 14:23:16 +0700

    add scraped data

--- tugasWebScrapping.py
+++ tugasWebScrapping.py
@@ -12,31 +12,35 @@ for page in range(1, 7):
     req = requests.get(URL + str(page) + '/')
     soup = bs(req.text, 'html.parser')
     titles = soup.find_all('h3', attrs={'class': 'archive-list__title'})
+    links = soup.find_all('a')
+    
     count = 1
     
-    for title in titles:
+    for title, link in zip(titles, links):
         d = {}
         d['Page Number'] = f'Page {page}'
         d['Title Number'] = f'Title {count}'
         d['Title Name'] = title.text
+        d['Link'] = link.get('href')
         titles_list.append(d)
         count += 1
     
     if page == 7:
         count = 1
-        for title in titles:
+        for title, link in zip(titles, links):
             e = {}
             e['Page Number'] = f'Page {page}'
             e['Title Number'] = f'Title {count}'
             e['Title Name'] = title.text
+            e['Link'] = link.get('href')
             count += 1
             title_page.append(e)
 
 titles_all = titles_list + title_page
 
-filename = 'hasilScrapping.csv'
+filename = input('Masukkan nama file: ')+'.csv'
 with open(filename, 'w', newline='') as f:
-    writer = csv.DictWriter(f, ['Page Number', 'Title Number', 'Title Name'])
+    writer = csv.DictWriter(f, ['Page Number', 'Title Number', 'Title Name', 'Link'])
     writer.writeheader()
     writer.writerows(titles_all)
 
